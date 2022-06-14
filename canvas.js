fc81cb7d4af4f48f65f54d0ae9ab777f4a98ec0b
horus <h.horace0921@gmail.com>
2022-06-14 23:49:24 +1000

    more drawing functions

--- canvas.js
+++ canvas.js
@@ -1,50 +1,90 @@
 // Canvas
-window.addEventListener("load", () => {
-  const canvas = document.querySelector("#canvas");
-  const cxt = canvas.getContext("2d");
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
 
-  // Resizing
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
+canvas.height = window.innerHeight / 2;
+canvas.width = window.innerWidth / 2;
 
-  // Variables
-  let painting = false;
+let penButton = document.querySelector("#pen");
+let eraserButton = document.querySelector("#eraser");
+let switchColorButton = document.querySelector("#color");
+let changeWidthButton = document.querySelector("#width");
+let clearButton = document.querySelector("#clear");
+let painting = false;
 
-  function startingPoint(e) {
-    painting = true;
-    draw(e);
-  }
+function startingPoint(e) {
+  painting = true;
+  draw(e);
+}
 
-  function endingPoint() {
-    painting = false;
-    cxt.beginPath();
-  }
+function endingPoint() {
+  painting = false;
+  ctx.beginPath();
+}
 
-  function draw(e) {
-    if (!painting) return;
+function draw(e) {
+  if (!painting) return;
 
-    cxt.lineWidth = 10;
-    cxt.lineCap = "round";
-    cxt.strokeStyle = "black";
+  penButton.addEventListener("click", pen);
+  eraserButton.addEventListener("click", eraser);
+  switchColorButton.addEventListener("change", switchColor);
+  changeWidthButton.addEventListener("change", changeWidth);
+  clearButton.addEventListener("click", clear);
 
-    cxt.lineTo(e.clientX, e.clientY);
-    cxt.stroke();
-    cxt.beginPath();
-    cxt.moveTo(e.clientX, e.clientY);
-  }
+  ctx.lineCap = "round";
 
-  // EventListeners
-  canvas.addEventListener("mousedown", startingPoint);
-  canvas.addEventListener("mouseup", endingPoint);
-  canvas.addEventListener("mousemove", draw);
-});
+  ctx.lineTo(e.clientX - ctx.canvas.offsetLeft, e.clientY - ctx.canvas.offsetTop);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(e.clientX - ctx.canvas.offsetLeft, e.clientY - ctx.canvas.offsetTop);
+}
+
+function pen() {
+  ctx.strokeStyle = "black";
+}
+
+function eraser() {
+  ctx.strokeStyle = "white";
+}
+
+function switchColor() {
+  ctx.strokeStyle = switchColorButton.value;
+}
+
+function changeWidth() {
+  ctx.lineWidth = changeWidthButton.value;
+}
+
+function clear() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+canvas.addEventListener("mousedown", startingPoint);
+canvas.addEventListener("mouseup", endingPoint);
+canvas.addEventListener("mousemove", draw);
 
 // window.addEventListener("resize", () => {
-//   canvas.height = window.innerHeight;
-//   canvas.width = window.innerwidth;
-// });
+//   canvas.height = window.innerHeight / 2;
+//   canvas.width = window.innerwidth / 2;
+// })
 
 // Random word generator
-fetch("https://random-word-api.herokuapp.com/word")
+function getWord() {
+  let wordField = document.querySelector("#wordField");
+
+  fetch("https://random-word-api.herokuapp.com/word")
     .then( res => res.json())
-    .then( json => console.log(json))
+    .then( json => (function () {
+      let randomWord = json[0].toUpperCase();
+      wordField.innerText = randomWord;
+    })())
+    .catch( err => function () {
+      console.log("ERROR!!!")
+    })
+}
+
+let switchWordButton = document.querySelector("#switchWord");
+
+switchWordButton.addEventListener("click", getWord);
+
+getWord();
